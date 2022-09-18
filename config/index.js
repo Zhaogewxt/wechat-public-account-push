03a0dc99b53322749a382808adf6796e9f81a391
Zhaogewxt <113818634+Zhaogewxt@users.noreply.github.com>
2022-09-18 16:28:04 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oBdbH6DhKTW38-ons6rjkTQtJNZM",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "Ef4zquNsxEhaARsVzLFET-tRPbImFfacPVPVJbXdrbs",
       // 所在省份
       province: "广东",
       // 所在城市
